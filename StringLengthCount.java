d63fabfc3d0be7ed92c1b5c6abd8ee6b8b30f8d4
vishkhi123 <vishalkhiratkar@gmail.com>
2024-02-20 12:43:10 +0530

    To Count Length of a String

---
+++ StringLengthCount.java
@@ -0,0 +1,9 @@
+//To Count Length of a String
+
+class StringLengthCount {
+    public static void main(String[] args) {
+        String abc = "Hello Buddy!!";
+
+        System.out.println(abc.length());
+    }
+}
